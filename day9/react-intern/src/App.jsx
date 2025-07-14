5def3c1f8b285460e8ec84c3d941f1b25e70e3f0
Nidhish <nidhish8102005@gmail.com>
2025-07-14 23:27:53 +0530

    Day 9 AN

--- day9/react-intern/src/App.jsx
+++ day9/react-intern/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
+import './App.css'
 import Login from './components/Login'
-
+// import Register from './components/Register'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-       <Login/>
+      <div>
+        <Login></Login>
+        {/* <Register/> */}
+      </div>
     </>
   )
 }
